fc979a550420262be3b5b155976c539d51d757a9
nanerseasac <renan.casaeses@gmail.com>
2023-07-12 21:58:19 -0300

    Done

---
+++ app.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('button')
+
+const popup = document.querySelector('.popup-wrapper')
+
+
+
+button.addEventListener('click', () => {
+   popup.style.display = 'block'
+})
+
+popup.addEventListener('click', event => {
+    const classNameOfClickedElement = event.target.classList[0]
+
+    const classNames = ['popup-wrapper', 'popup-link', 'popup-close']
+    const shouldClosePopup = classNames.some(className => className === classNameOfClickedElement)
+    if(shouldClosePopup) {
+        popup.style.display = 'none'
+    }
+    // popup.style.display = 'none'
+
+})
